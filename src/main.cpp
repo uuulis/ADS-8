b0265a70b5b4d057ed3cc33bfc6e3f920af3a1fc
uuulis <listratovaul@gmail.com>
2026-05-31 01:36:04 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <iostream>
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
 
 int main() {
+    BST<std::string> tree;
+    makeTree(tree, "src/war_peace.txt");
+    std::cout << "Tree depth: " << tree.depth() << std::endl;
+    printFreq(tree);
     return 0;
 }
